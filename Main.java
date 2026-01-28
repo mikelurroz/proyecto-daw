b66ab69f2eacda3c452b4ee24f8fe443828c000c
Mikel Valmaseda Garcia <ikmikelvalmaseda@H014451.NET>
2026-01-28 09:08:53 +0100

    Commit de MikelValma

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        String nombre = "FETCH";
-        String apellido = "Garcia";
+        String nombre = "Mikel";
+        String apellido = "Valma";
 
         String saludo = "Hola " + nombre + " " + apellido + " ¿Que tal?";
 
